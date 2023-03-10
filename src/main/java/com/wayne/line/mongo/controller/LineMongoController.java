c2562b456445541ed271d83baec75019434af242
DESKTOP-UGQ2HN4\Wego Lin <wayne2347@gmail.com>
2023-03-10 11:41:24 +0800

    Change the Open-api-doc content

--- src/main/java/com/wayne/line/mongo/controller/LineMongoController.java
+++ src/main/java/com/wayne/line/mongo/controller/LineMongoController.java
@@ -1,23 +1,21 @@
 package com.wayne.line.mongo.controller;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.model.PushMessage;
-import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.response.BotApiResponse;
 import com.wayne.line.mongo.service.LineMessageItemService;
 import com.wayne.line.mongo.utils.LineBotClient;
@@ -26,7 +24,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import sun.util.logging.resources.logging;
 
 @RestController
 @RequestMapping("/line/mongo/message")
@@ -44,7 +41,9 @@ public class LineMongoController {
 	@Operation(summary = "Push Message Back to Line")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Message Send OK!", 
-					content = {@Content(mediaType = MediaType.TEXT_PLAIN_VALUE)})
+					content = {@Content(mediaType = MediaType.TEXT_PLAIN_VALUE)}),
+			@ApiResponse(responseCode = "500", description = "Fail! Internal Error", 
+				content = {@Content(mediaType = MediaType.TEXT_PLAIN_VALUE)}),
 	})
 	public String sendMessageBackToLine(@RequestParam(required = true) String sendMessages, @RequestParam(required = true) String userId) { //		        "U93f0c12e0a2aeabdf6309aa70d2f95da"
 		
@@ -55,9 +54,12 @@ public class LineMongoController {
 
 
 	@GetMapping(value = "/list")
-	@Operation(summary = "List the ")
+	@Operation(summary = "List the Specific User Converation Records")
 	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Message query successfully!")
+			@ApiResponse(responseCode = "200", description = "Message query successfully!",
+					content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)} ),
+			@ApiResponse(responseCode = "500", description = "Fail! Internal Error", 
+			content = {@Content(mediaType = MediaType.TEXT_PLAIN_VALUE)}),
 	})
 	public List<String> listUserMessages(@RequestParam(required = true) String userId) {
 		return lineMessageItemService.findAllByUserId(userId).stream()
@@ -66,8 +68,10 @@ public class LineMongoController {
 	}
 
     @ExceptionHandler(Exception.class)
-    public String handleException(Exception e) {
+    public ResponseEntity<Object> handleException(Exception e) {
     	logger.error(e.getMessage());
-		return "Failed!";
+		return new ResponseEntity<Object>(e.getMessage(),
+										  new HttpHeaders(),
+										  HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
